7e349e0c461b70635f0e40c18883fc89e2a5d943
Opilane TTHK <opilane@tthk.ee>
2020-11-18 09:30:27 +0200

    Add project files.

---
+++ CarouselAppDovzenok/CarouselAppDovzenok/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CarouselAppDovzenok
+{
+    public partial class MainPage : CarouselPage
+    {
+        public MainPage()
+        {
+            var redContentPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                    new Label
+                    {   Text="Red",
+                        FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                        HorizontalOptions = LayoutOptions.Center},
+
+                    new BoxView
+                    {   Color = Color.Red,
+                        WidthRequest = 200,
+                        HeightRequest = 200,
+                        HorizontalOptions = LayoutOptions.Center,
+                        VerticalOptions = LayoutOptions.CenterAndExpand}
+                    }
+                }
+            };
+            var greenContentPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                    new Label
+                    {   Text="Green",
+                        FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                        HorizontalOptions = LayoutOptions.Center},
+
+                    new BoxView
+                    {   Color = Color.Green,
+                        WidthRequest = 200,
+                        HeightRequest = 200,
+                        HorizontalOptions = LayoutOptions.Center,
+                        VerticalOptions = LayoutOptions.CenterAndExpand}
+                    }
+                }
+            };
+            var blueContentPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                    new Label
+                    {   Text="Blue",
+                        FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                        HorizontalOptions = LayoutOptions.Center},
+
+                    new BoxView
+                    {   Color = Color.Blue,
+                        WidthRequest = 200,
+                        HeightRequest = 200,
+                        HorizontalOptions = LayoutOptions.Center,
+                        VerticalOptions = LayoutOptions.CenterAndExpand}
+                    }
+                }
+            };
+            Children.Add(redContentPage);
+            Children.Add(greenContentPage);
+            Children.Add(blueContentPage);
+        }
+    }
+}
